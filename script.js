9af1463cb51f0dbcbd7c3fe69df9e8de9a695c56
dimgg-maker <dimasrizal1122@gmail.com>
2025-10-21 11:11:43 +0000

    update web

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
-document.getElementById("alertButton").addEventListener("click", () => {
-  alert("Tombol diklik!");https://bit.ly/dimbrrr
+document.addEventListener("DOMContentLoaded", function() {
+  const tombol = document.getElementById("alertButton");
+  tombol.addEventListener("click", () => {
+    alert("Tombol diklik! Sekarang kamu akan diarahkan...");
+    window.location.href = "https://dimgg-maker.github.io/HUAAAA/";
+  });
 });
